2185bb10e7f95e750947aab5178f08a4f712a2ce
unknown <loreta.padaigaite@codeacademylt.onmicrosoft.com>
2023-01-31 22:07:20 +0200

    database config

--- app/app.js
+++ app/app.js
@@ -1,6 +1,17 @@
-const express = require('express')
+const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
+
+const mysqlConfig = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: '',
+    port: 3306
+}
+
+const connection = mysql.createConnection(mysqlConfig);
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Express server running on PORT:${PORT}`))
